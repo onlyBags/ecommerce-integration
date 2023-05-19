8c46701668485cece69368607a76e495ca38b365
pvtallulah <maikinahara@gmail.com>
2023-05-19 19:26:45 -0300

    fix tests 2

--- packages/ecommerce-config/src/lib/ecommerce-config.ts
+++ packages/ecommerce-config/src/lib/ecommerce-config.ts
@@ -1,19 +1,21 @@
 import { config } from 'dotenv';
-import { ConnectOptions, DatabaseType } from 'typeorm';
+import { DatabaseType } from 'typeorm';
 
-config();
+config({ path: process.cwd() + '/../../.env' });
 
 export const envConfig = Object.freeze({
   port: parseInt(process.env.PORT || '8080'),
-  cypherKey: process.env.CYPHER_KEY,
-  cypherIV: process.env.CYPHER_IV,
-  cypherAlgorithm: process.env.CYPHER_ALGORITHM,
+  cypherKey: process.env.CYPHER_KEY || '',
+  cypherIV: process.env.CYPHER_IV || '',
+  cypherAlgorithm: process.env.CYPHER_ALGORITHM || '',
   dbType: process.env.DB_TYPE as DatabaseType,
-  dbHost: process.env.DB_HOST,
+  dbHost: process.env.DB_HOST || 'localhost',
   dbPort: parseInt(process.env.DB_PORT || '3306'),
-  dbUsername: process.env.DB_USERNAME,
-  dbPassword: process.env.DB_PASSWORD,
-  dbDatabase: process.env.DB_DATABASE,
+  dbUsername: process.env.DB_USERNAME || 'root',
+  dbPassword: process.env.DB_PASSWORD || '',
+  dbDatabase: process.env.DB_DATABASE || 'ecommerce',
+  magentoTestConsumerKey: process.env.MAGENTO_TEST_CONSUMER_KEY || '',
+  magentoTestConsumerSecret: process.env.MAGENTO_TEST_CONSUMER_SECRET || '',
 });
 
 export default envConfig;
