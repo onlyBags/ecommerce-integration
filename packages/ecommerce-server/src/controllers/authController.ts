d808283e97b352b020eab6c950fba38714b775d6
pvtallulah <maikinahara@gmail.com>
2024-10-16 08:56:23 -0300

    Update authController.ts

--- packages/ecommerce-server/src/controllers/authController.ts
+++ packages/ecommerce-server/src/controllers/authController.ts
@@ -65,8 +65,4 @@ export class AuthController extends Controller {
     }
   }
 }
-// {
-//   "message": "Logins",
-//   "address": "0xdaC8d078646f1a0Edf3dc700e044fA57eF17b928",
-//   "signature": "0xfb1b50e6bc78f6d6acf42a54304f8824e07796c2723beeac3f17c7d02d304a1110d6b32d819da8714e5514d11cb791e52d710b7014c7efebb24a78b0433c30471b"
-// }
+
